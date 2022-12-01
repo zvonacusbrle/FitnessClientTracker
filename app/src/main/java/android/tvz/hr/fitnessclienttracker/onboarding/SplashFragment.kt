f59f3cd565f110e700804c89f4f8bd5c671f5434
zbrle <zbrlekovic@gmail.com>
2022-12-01 14:52:08 +0100

    Add SplashFragment with logo photo and update gitignore

---
+++ app/src/main/java/android/tvz/hr/fitnessclienttracker/onboarding/SplashFragment.kt
@@ -0,0 +1,26 @@
+package android.tvz.hr.fitnessclienttracker.onboarding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Text(text = "Hrvatska 2:1 Belgija")
+            }
+        }
+    }
+
+
+}
