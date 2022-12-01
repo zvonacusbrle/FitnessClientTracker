fd254936b99109845a6263ee8205fd7e336e4781
zbrle <zbrlekovic@gmail.com>
2022-12-01 15:10:37 +0100

    Make animation for splash screen with logo picture

--- app/src/main/java/android/tvz/hr/fitnessclienttracker/onboarding/SplashFragment.kt
+++ app/src/main/java/android/tvz/hr/fitnessclienttracker/onboarding/SplashFragment.kt
@@ -1,12 +1,30 @@
 package android.tvz.hr.fitnessclienttracker.onboarding
 
 import android.os.Bundle
+import android.tvz.hr.fitnessclienttracker.R
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material.Text
+import android.view.animation.OvershootInterpolator
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.animation.core.tween
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.compose.ui.Modifier
+import androidx.compose.animation.core.Animatable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
 
 class SplashFragment : Fragment() {
 
@@ -17,8 +35,40 @@ class SplashFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                Text(text = "Hrvatska 2:1 Belgija")
+                Surface(color = colorResource(R.color.bg_color), modifier = Modifier.fillMaxSize()) {
+                    SplashScreen()
+                }
             }
+        }
+    }
+
+    @Composable
+    fun SplashScreen() {
+        val scale = remember {
+            Animatable(0f)
+        }
+        LaunchedEffect(key1 = true) {
+            scale.animateTo(
+                targetValue = 0.7f,
+                animationSpec = tween(
+                    durationMillis = 800,
+                    easing = {
+                        OvershootInterpolator(4f).getInterpolation(it)
+                    }
+                )
+            )
+            delay(3000L)
+        }
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.fitness_client_tracker),
+                contentDescription = "Logo",
+                modifier = Modifier.scale(scale.value)
+            )
         }
     }
 
